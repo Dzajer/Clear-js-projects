4ddca4c6257d2dffe4870889be2d000241f32c53
USER-KOMPUTER\user <dzajer2017@gmail.com>
2021-02-04 18:51:22 +0100

    Update script.js

--- Stoper v2/script.js
+++ Stoper v2/script.js
@@ -1,21 +1,25 @@
 const startBtn = document.querySelector(".start");
-const pausetBtn = document.querySelector(".pause");
+const pauseBtn = document.querySelector(".pause");
 const stopBtn = document.querySelector(".stop");
 const resetBtn = document.querySelector(".reset");
+const historyBtn = document.querySelector(".history");
 const stopwatch = document.querySelector(".stopwatch");
 const time = document.querySelector(".time");
 const timeList = document.querySelector(".time-list");
+
 const infoBtn = document.querySelector(".info");
 const modalShadow = document.querySelector(".modal-shadow");
 const closeModalBtn = document.querySelector(".close");
-const historyBtn = document.querySelector(".history");
+
+let timesArr = [];
+
 let countTime;
 let minutes = 0;
 let seconds = 0;
 
-let timesArr = [];
 const handleStart = () => {
   clearInterval(countTime);
+
   countTime = setInterval(() => {
     if (seconds < 9) {
       seconds++;
@@ -28,20 +32,22 @@ const handleStart = () => {
       seconds = 0;
       stopwatch.textContent = `${minutes}:00`;
     }
-  }, 10);
+  }, 1000);
 };
+
 const handleStop = () => {
   time.innerHTML = `Ostatni czas: ${stopwatch.textContent}`;
   if (stopwatch.textContent !== "0:00") {
     time.style.visibility = "visible";
     timesArr.push(stopwatch.textContent);
   }
-
   clearStuff();
 };
+
 const handlePause = () => {
   clearInterval(countTime);
 };
+
 const handleReset = () => {
   time.style.visibility = "hidden";
   timesArr = [];
@@ -50,10 +56,10 @@ const handleReset = () => {
 
 const clearStuff = () => {
   clearInterval(countTime);
-  stopwatch.textContent = "0:00";
+  stopwatch.textContent = `0:00`;
   timeList.textContent = "";
-  seconds = 0;
   minutes = 0;
+  seconds = 0;
 };
 
 const showHistory = () => {
@@ -61,7 +67,7 @@ const showHistory = () => {
   let num = 1;
   timesArr.forEach((time) => {
     const newTime = document.createElement("li");
-    newTime.innerHTML = `Pomiar nr ${num}: <span>${time}</span>`;
+    newTime.innerHTML = `Pomiar nr #${num}: <span>${time}</span>`;
 
     timeList.appendChild(newTime);
     num++;
@@ -76,19 +82,14 @@ const showModal = () => {
   }
   modalShadow.classList.toggle("modal-animation");
 };
-pausetBtn.addEventListener("click", handlePause);
 
 startBtn.addEventListener("click", handleStart);
-
+pauseBtn.addEventListener("click", handlePause);
 stopBtn.addEventListener("click", handleStop);
-
 resetBtn.addEventListener("click", handleReset);
-
 historyBtn.addEventListener("click", showHistory);
-
 infoBtn.addEventListener("click", showModal);
-
 closeModalBtn.addEventListener("click", showModal);
-window.addEventListener("click", (e) => {
-  e.target === modalShadow ? showModal() : false;
-});
+window.addEventListener("click", (e) =>
+  e.target === modalShadow ? showModal() : false
+);
